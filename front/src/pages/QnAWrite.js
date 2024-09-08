2ed888425c1dd86b8cb48448c29e2082c76b50bc
Jin Minu <s9430939@naver.com>
2024-09-08 14:13:11 +0900

    코드블럭 추가

--- front/src/pages/QnAWrite.js
+++ front/src/pages/QnAWrite.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { getUserInfo } from "../utils/auth";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { prism } from "react-syntax-highlighter/dist/esm/styles/prism"; // 스택오버플로우 유사 스타일
+import Markdown from "markdown-to-jsx";
+
 import "./QnAWrite.css";
 
 function WriteQnAPost() {
@@ -81,6 +85,18 @@ function WriteQnAPost() {
             onChange={(e) => setContent(e.target.value)}
             required
           />
+        </div>
+        <div className="form-field">
+          <label htmlFor="code">코드:</label>
+          <textarea
+            id="code"
+            placeholder="백틱 세 번으로 코드 블록을 입력하세요. 예: ```코드```"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </div>
+        <div className="markdown-preview">
+          <Markdown>{content}</Markdown>
         </div>
         <button type="submit">제출</button>
       </form>
